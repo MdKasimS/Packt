1f538294fb8ac3286e0d1bf245ed7560d39ca87a
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2024-06-11 21:12:39 +0530

    Db connection string added.

--- Shared/Northwind.cs
+++ Shared/Northwind.cs
@@ -10,7 +10,17 @@ namespace Packt.Shared
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // base.OnConfiguring(optionsBuilder);
-            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "No");
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
+
+            optionsBuilder.UseSqlite(path);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().
+            Property(Category => Category.CategoryName)
+            .IsRequired()
+            .HasMaxLength(15);
         }
     }
 }
